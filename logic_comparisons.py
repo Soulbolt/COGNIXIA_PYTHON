b75af1f5847bca73eb53d558a14f22cb885bc930
Soulbolt <soulbolt@gmail.com>
2021-02-24 14:10:50 -0700

    Initial commit for logic_comparisons.py

---
+++ logic_comparisons.py
@@ -0,0 +1,16 @@
+
+print(5 < 10)
+print(5 < 5)
+print(5 == 5)
+print(5 != 5)
+print(5 <= 5)
+
+#if kwame_pts < kelby_pts:
+ #   print("kwame wins!")
+
+ print(True and True)   #True
+ print(True and False)  #False
+ print(True or False)   #True
+ print(not True)        #False
+
+ 
