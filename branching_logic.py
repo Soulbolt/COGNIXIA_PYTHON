76ac39313a0047296e0a9bf1b52a60939dda57ad
Soulbolt <soulbolt@gmail.com>
2021-02-24 14:04:20 -0700

    Initial commit for branching_logic.py

---
+++ branching_logic.py
@@ -0,0 +1,14 @@
+like_python = True
+like_netflix = False
+
+if like_python:
+    print("I will study now")
+
+elif like_netflix:
+    print("I will relax now") #elif triggers as long as above statements are false
+
+else:
+    print("if everything is false above me, this prints")
+
+
+    
