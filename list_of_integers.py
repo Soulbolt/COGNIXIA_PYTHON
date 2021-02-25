ab7a92ef55db9bd06858d9cbc0f880bb1b50cb56
Soulbolt <soulbolt@gmail.com>
2021-02-25 00:03:54 -0700

    Sorted array list from small to highest number.. not quite the output I need.. but getting there. list_of_integers.py

--- list_of_integers.py
+++ list_of_integers.py
@@ -1,11 +1,13 @@
-#Write a function that takes a list of integers and palces the zeroes
+#Write a function that takes a list of integers and places the zeroes
 #in the center of the list. All non-zero integers should keep their
 #relative position. Center is defined as floor(length of list / 2).
 
-center_zeroes([1, 1, 3, 0, 6, 0])
-center_zeroes([0, 3, 1])
-center_zeroes([1, 1, 3, 0])
-center_zeroes([1, 1, 3, 0, 6, 0])
+def centered(list_num):
+    list_num.sort()
+    print(list_num)
 
-def centered(center_zeroes):
+centered(list_num = [1, 1, 3, 0, 6, 0])
+centered(list_num = [0, 3, 1])
+centered(list_num = [1, 1, 3, 0])
+centered(list_num = [1, 1, 3, 0, 6, 0])
     
