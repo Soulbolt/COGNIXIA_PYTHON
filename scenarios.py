8451bdfb28c0576d5bbb9387d9e6ece06143476f
Soulbolt <soulbolt@gmail.com>
2021-02-25 09:43:57 -0700

    Made improvements by using for loop and adding test case into a list to iterate through it for scenarios.py

--- scenarios.py
+++ scenarios.py
@@ -1,15 +1,16 @@
-
+test_case = [1, 2, 3, 4, 6, 8, 11, 18, 20, 22]
 
 def weird_not_weird():
-    num1 = int(input("Please enter number : "))     #ask user for input
+    for num1 in test_case:    
+        #num1 = int(input("Please enter number : "))     #ask user for input
 
-    if num1 % 2 != 0:                             #if num1 is odd print Weird
-        print("Weird")        
-    elif num1 % 2 == 0 and num1 in range(2,5):    #num1 within range of 2 - 5
-            print("Not weird")                      #if num1 is even and within range of 2 -5 print Not Weird
-    elif num1 % 2 == 0 and num1 in range(6,20):   #num1 within range of 6-20
-            print("Weird")                          #if num1 is even and within range of 6-20 print Weird
-    elif num1 % 2 == 0 and num1 >= 20:            #if num1 is even and geater than 20 print Not Weird
-            print("Not Weird")
+        if num1 % 2 != 0:                             #if num1 is odd print Weird
+                print("Weird")        
+        if num1 in range(2,5):    #num1 within range of 2 - 5
+                print("Not weird")                      #if num1 is even and within range of 2 -5 print Not Weird
+        if num1 in range(6,20):   #num1 within range of 6-20
+                print("Weird")                          #if num1 is even and within range of 6-20 print Weird
+        if num1 > 20:            #if num1 is even and greater than 20 print Not Weird
+                print("Not Weird")
 
 weird_not_weird()                                   #call Function
