072418f1cd15ce29e43a8d71e176c5c829cea720
Soulbolt <soulbolt@gmail.com>
2021-02-25 10:15:03 -0700

    Solution with while loop for fibonacci.2py

---
+++ fibonacci2.py
@@ -0,0 +1,14 @@
+def fibonacci():
+    i = 0
+    while i <= 999:
+        i += 1
+        if i % 3 == 0 and i % 5 ==0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 ==0:
+            print("Buzz")
+        else:
+            print(i)
+
+fibonacci()
