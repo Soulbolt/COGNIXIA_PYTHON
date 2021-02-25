d45677ee23b031e11b1ef48e3358770800f068dc
Soulbolt <soulbolt@gmail.com>
2021-02-24 17:22:24 -0700

    Modified code and resolve issue with string convertion by adding int type when asking user for input. Function and test case work as intended.

--- scenarios.py
+++ scenarios.py
@@ -1,16 +1,15 @@
 
 
-def weird_not_weird(num1):
-    if num1 % 2 != 0:
-        print("Weird")    
-    for num1 in range(2,5):
-        if num1 % 2 == 0:
-            print("weird")
-    for num1 in range(6,20):
-        if num1 % 2 == 0:
-            print("Weird")
-        elif num1 % 2 == 0 & num1 < 20:
-            print("not Weird")
+def weird_not_weird():
+    num1 = int(input("Please enter number : "))     #ask user for input
 
-num1 = input("Please enter number : ")
-print(weird_not_weird(num1))
+    if num1 % 2 != 0:                             #if num1 is odd print Weird
+        print("Weird")        
+    elif num1 % 2 == 0 and num1 in range(2,5):    #num1 within range of 2 - 5
+            print("Not weird")                      #if num1 is even and within range of 2 -5 print Not Weird
+    elif num1 % 2 == 0 and num1 in range(6,20):   #num1 within range of 6-20
+            print("Weird")                          #if num1 is even and within range of 6-20 print Weird
+    elif num1 % 2 == 0 and num1 >= 20:            #if num1 is even and geater than 20 print Not Weird
+            print("Not Weird")
+
+weird_not_weird()                                   #call Function
