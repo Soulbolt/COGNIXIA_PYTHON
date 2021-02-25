7a3fe2623495eb039fe41b0128072cfaf93f546a
Soulbolt <soulbolt@gmail.com>
2021-02-25 13:45:09 -0700

    updated some syntax for classes.intro.py

--- classes_intro.py
+++ classes_intro.py
@@ -1,4 +1,4 @@
-class car():
+class Car():
 
     def __init__(self, color, make, year, HP):
         self.color = color
@@ -16,7 +16,7 @@ class car():
 my_car = Car("blue", "Ford", 2020, 7)
 your_car = Car("red", "Tesla", 2020, 8)
 
-my_car.start_car()
+#my_car.start_car()
 
 print(my_car < your_car)
-print(my_car <= your_car)
+#print(my_car <= your_car)
