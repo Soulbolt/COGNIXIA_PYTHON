d2c94f017c5485b22381692ba8a685fd51cc8ba0
Soulbolt <soulbolt@gmail.com>
2021-02-24 19:01:40 -0700

    Initial commit for list_of_integers.py

---
+++ list_of_integers.py
@@ -0,0 +1,11 @@
+#Write a function that takes a list of integers and palces the zeroes
+#in the center of the list. All non-zero integers should keep their
+#relative position. Center is defined as floor(length of list / 2).
+
+center_zeroes([1, 1, 3, 0, 6, 0])
+center_zeroes([0, 3, 1])
+center_zeroes([1, 1, 3, 0])
+center_zeroes([1, 1, 3, 0, 6, 0])
+
+def centered(center_zeroes):
+    
